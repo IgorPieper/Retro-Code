3c70e572e74df7200c35702d8bbfb5bf4b6d13cb
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2025-03-12 00:35:13 +0100

    Update Kino6.php

--- Web/Lab2/05_Modify_Movie/Kino6.php
+++ Web/Lab2/05_Modify_Movie/Kino6.php
@@ -1,15 +1,15 @@
 <?php
 
-echo "<h1><center>Punkt 5</center></h1>";
+echo "<h1><center>Punkt 6</center></h1>";
 
-$numer = $_POST["numer"];
+$film = $_POST["film"];
 $zmien = $_POST["zmien"];
 $nowe = $_POST["nowe"];
 
 $conf = new mysqli("localhost", "root", "", "kino") 
     or die("Nie ma kina przez COVIDa, więc zamawiaj jedzenie z Gowida");
 
-$wynik = $conf->query("UPDATE klient SET $zmien='$nowe' WHERE id='$numer'");
+$wynik = $conf->query("UPDATE film SET $zmien='$nowe' WHERE id='$film'");
 
 echo "Zrobione!!!!";
 
