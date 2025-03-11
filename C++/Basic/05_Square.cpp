cf41519af4eea710f8d139db006089c5a337832f
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2025-03-11 23:09:21 +0100

    Create 05_Square.cpp

---
+++ C++/Basic/05_Square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int dlugosc;
+
+int main() {
+    cout << "Podaj dlugosc kwadratu: " << endl;
+    cin >> dlugosc;
+
+    int pole = dlugosc * dlugosc;
+
+    cout << "Pole kwadratu wynosi: " << pole << endl;
+
+    return 0;
+}
